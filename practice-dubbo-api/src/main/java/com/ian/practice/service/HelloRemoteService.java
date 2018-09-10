82dd844a1c59a96e6b7945253722118a7eedc9c2
Ian1002 <littlej1002@163.com>
2018-09-10 17:06:45 +0800

    增加dubbo api项目

---
+++ practice-dubbo-api/src/main/java/com/ian/practice/service/HelloRemoteService.java
@@ -0,0 +1,18 @@
+package com.ian.practice.service;
+
+/**
+ * HelloRomteService
+ *
+ * @author YangJing
+ * @date 2018/9/6 11:29
+ */
+public interface HelloRemoteService {
+
+    /**
+     * hello
+     *
+     * @param name
+     * @return
+     */
+    String hello(String name);
+}
